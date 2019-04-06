5dc19796fb92bbe90f77f56a73a9b56553e921be
Lam Tu <lam.tu@xero.com>
2019-04-06 17:59:14 +1100

    Initial Commit

---
+++ ToWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToWatch
+//
+//  Created by Lam Tu on 6/4/19.
+//  Copyright © 2019 Lam Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
